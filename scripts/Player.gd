56d372ab68f01e9904e61f9ac65f8a9ec17b2512
Rafael Ramirez <takeshie01@gmail.com>
2023-08-27 00:31:50 -0400

    First commit
    project initialization

---
+++ scripts/Player.gd
@@ -0,0 +1,75 @@
+extends CharacterBody3D
+
+var speed
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.003
+
+#bob variables
+const BOB_FREQ = 2.4
+const BOB_AMP = 0.08
+var t_bob = 0.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 9.8
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+@onready var camera3 = $Head/Camera3D2
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		if camera.is_current():
+			camera.rotate_x(-event.relative.y * SENSITIVITY)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		else:
+			camera3.rotate_x(-event.relative.y * SENSITIVITY)
+			camera3.rotation.x = clamp(camera3.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	#Handle sprint
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+	#Handle camera
+	_change_camera()
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = 0.0
+		velocity.z = 0.0
+		
+	#head bobbing
+	#Implement later on
+
+	move_and_slide()
+
+func _change_camera():
+	if Input.is_action_just_pressed("camera"):
+		if camera.is_current():
+			camera3.set_current(true)
+			camera.set_current(false)
+		else:
+			camera.set_current(true)
+			camera3.set_current(false)
